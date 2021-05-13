4a5195419553ce771a35a4a8e194fecf15325733
SJomurodov <shah9409@gmail.com>
2021-05-13 16:16:49 +0500

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -88,7 +88,7 @@ if ($text == "/start") {
     $xabar = $telegram->sendInvoice($content);
     $xabar_msg = $xabar["result"]["message_id"];
 
-    if ($xabar_msg) {
+    if (!$xabar_msg) {
         $content = ['chat_id' => $chat_id, 'text' => "Siz @BotFather dan kirib Payment sozlamasini sozlaganingiz aniqmi?", 'parse_mode' => 'markdown'];
         $telegram->sendMessage($content);
     }
